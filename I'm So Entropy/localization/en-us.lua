ee931fb6b183ec6afcb949b97582e64981249429
PokerThePoker <kowyn865@gmail.com>
2025-06-18 17:53:03 -0300

    uhh does this work
    please man

---
+++ I'm So Entropy/localization/en-us.lua
@@ -0,0 +1,39 @@
+return {
+	descriptions = {
+		Joker = {
+			j_ise_the_fucking_sun
+			= {
+				name = "The Fucking Sun",
+				text = {
+					"{C:gold}+#1#{} Ascension Power",
+				},
+			},
+			j_ise_bright_joker
+			= {
+				name = "Bright Joker",
+				text = {
+					"{C:gold}+#1#{} Ascension Power",
+					"if played hand contains",
+					"a {C:attention}Pair{}"
+				},
+			},
+			j_ise_accelerated_backwards_hopping
+			= {
+				name = "Accelerated Backwards Hopping",
+				text = {
+					"{C:attention}+#1#{} operator to",
+					"{C:attention}Dekatria{}"
+				},
+			},
+			j_ise_sunrise
+			= {
+				name = "Sunrise",
+				text = {
+					"{C:gold}+#1#{} Ascension Power",
+					"{C:gold}+1{} Ascension Power at",
+					"end of round",
+				},
+			},
+		},
+	},
+}
