b5c0305fdae0783914db1a60231fcb053930f20d
PokerThePoker <kowyn865@gmail.com>
2025-06-18 17:54:40 -0300

    fuck it didnt work
    i HATE this shit

--- I'm So Entropy/main.lua
+++
@@ -1,8 +0,0 @@
--- a file specificlaly to load all other lua files in the mod (and for config stuff)
-
-local files = {
-	"joker"
-}
-for i, v in pairs(files) do
-	assert(SMODS.load_file(v..".lua"))()
-end
